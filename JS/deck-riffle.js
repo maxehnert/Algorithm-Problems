af0935998b3f5256dbf24bd35c69402a942671b4
Max Ehnert <max.ehnert@gmail.com>
2015-08-20 17:50:33 -0400

    added deck-shuffle riffle(incomplete)

---
+++ JS/deck-riffle.js
@@ -0,0 +1,151 @@
+/*
+I figured out how to get rich: online poker.
+I suspect the online poker game I'm playing shuffles cards by doing a single " riffle ↴ ."
+
+To prove this, let's write a function to tell us if a full deck of cards shuffled_deck is a single riffle of two other halves half1 and half2.
+
+We'll represent a stack of cards as an array of integers in the range 1..521..52 (since there are 5252 distinct cards in a deck).
+
+Why do I care? A single riffle is not a completely random shuffle. If I'm right, I can make more informed bets and get rich and finally prove to my ex that I am not a "loser with an unhealthy cake obsession" (even though it's too late now because she let me go and she's never getting me back).
+
+Gotchas
+Watch out for index out of bounds errors! Will your function ever try to grab the 0th item from an empty array, or the nth item from an array with n elements (where the last index would be n-1n−1)?
+
+We can do this in O(n)O(n) time and O(1)O(1) additional space.
+
+Did you come up with a recursive solution? Keep in mind that you may be incurring a hidden space cost (probably O(n)O(n)) in the call stack ↴ ! You can avoid this using an iterative approach.
+
+Breakdown
+How can we re-phrase this problem in terms of smaller subproblems?
+
+Breaking the problem into smaller subproblems will clearly involve reducing the size of at least one of our stacks of cards. So to start, let's try taking the first card out of shuffled_deck.
+
+What should be true of this card if shuffled_deck is a riffle of half1 and half2?
+
+If shuffled_deck is a riffle of half1 and half2, then the first card from shuffled_deck should be either the same as the first card from half1 or the same as the first card from half2.
+
+Now that we know the first card checks out, how do we get to our subproblem?
+
+Let's "throw out" the top card from shuffled_deck as well as the card it matched with from the top of half1 or half2. Those cards are now "accounted for."
+
+Now we're left with a smaller version of the original problem, which we can solve using the same approach! So we keep doing this over and over until we exhaust shuffled_deck. If we get to the end and each card "checks out," we return True.
+
+
+Solution
+
+We walk through shuffled_deck, seeing if each card so far could have come from a riffle of the other two halves. To check this, we:
+
+Keep pointers to the current index in half1, half2, and shuffled_deck.
+Walk through shuffled_deck from beginning to end.
+If the current card in shuffled_deck is the same as the top card from half1, increment half1_index and keep going. This can be thought of as "throwing out" the top cards of half1 and shuffled_deck, reducing the problem to the remaining cards in the stacks.
+Same as above with half2.
+If the current card isn't the same as the card at the top of half1 or half2, we know we don't have a single riffle.
+If we make it all the way to the end of shuffled_deck and each card checks out, we know we do have a single riffle.
+
+  def is_single_riffle(half1, half2, shuffled_deck):
+    half1_index = 0
+    half2_index = 0
+    half1_max_index = len(half1) - 1
+    half2_max_index = len(half2) - 1
+
+    for card in shuffled_deck:
+        # if we still have cards in half1
+        # and the "top" card in half1 is the same
+        # as the top card in shuffled_deck
+        if half1_index <= half1_max_index and \
+            card == half1[half1_index]:
+            half1_index += 1
+
+        # if we still have cards in half2
+        # and the "top" card in half2 is the same
+        # as the top card in shuffled_deck
+        elif half2_index <= half2_max_index and \
+            card == half2[half2_index]:
+            half2_index += 1
+
+        # if the top card in shuffled_deck doesn't match the top
+        # card in half1 or half2, this isn't a single riffle.
+        else:
+            return False
+
+    # all cards in shuffled_deck have been "accounted for"
+    # so this is a single riffle!
+    return True
+
+Complexity
+O(n)O(n) time and O(1)O(1) additional space.
+*/
+
+
+var deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52];
+
+// First half of the deck
+var half1 = deck.slice(0, deck.length/2);
+
+// Second half of the deck
+var half2 = deck.slice(26, deck.length);
+
+var shuffled_arr_deck = half1.map(function (e, i) {
+    return [half1[i], half2[i]];
+});
+var shuffled_deck = shuffled_arr_deck.toString().split(',').map(Number);
+
+// shuffled_deck = [1, 27, 2, 28, 3, 29, 4, 30, 5, 31, 6, 32, 7, 33, 8, 34, 9, 35, 10, 36, 11, 37, 12, 38, 13, 39, 14, 40, 15, 41, 16, 42, 17, 43, 18, 44, 19, 45, 20, 46, 21, 47, 22, 48, 23, 49, 24, 50, 25, 51, 26, 52]
+
+/* Check if the Deck is a single rifle*/
+function is_single_riffle(half1, half2, shuffled_deck) {
+
+  var card;
+  var half1_index = 0;
+  var half2_index = 0;
+  var half1_max_index = half1.length - 1;
+  var half2_max_index = half2.length - 1;
+
+  for ( index in shuffled_deck ) {
+    console.log(index);
+    console.log('((half1[half1_index])-1) '+ ((half1[half1_index])-1));
+    console.log('((half2[half2_index])-1) '+ ((half2[half2_index])-1))
+
+    // 0
+    // ((half1[half1_index])-1) 0
+    // ((half2[half2_index])-1) 26
+    // it ran
+
+    // 1
+    // ((half1[half1_index])-1) 1
+    // ((half2[half2_index])-1) 26
+    // it ran
+
+    // 25
+    // ((half1[half1_index])-1) 25
+    // ((half2[half2_index])-1) 26
+    // it ran
+
+    // 26
+    // ((half1[half1_index])-1) NaN
+    // ((half2[half2_index])-1) 26
+    // it ran also
+
+    // 51
+    // ((half1[half1_index])-1) NaN
+    // ((half2[half2_index])-1) 51
+    // it ran also
+
+    if ( half1_index <= half1_max_index && index == ((half1[half1_index])-1) ) {
+            console.log('it ran');
+      half1_index += 1;
+    } else if ( half2_index <= half2_max_index && index == ((half2[half2_index])-1) ) {
+           console.log('it ran also');
+
+      half2_index += 1;
+    } else {
+                  console.log('shiiiit');
+
+      return false;
+    }
+
+  }
+  return true;
+}
+
+is_single_riffle(half1, half2, shuffled_deck);
