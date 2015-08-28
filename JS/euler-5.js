37280174770e8560f3756f1a3c1e729b12e5ebeb
Max Ehnert <max.ehnert@gmail.com>
2015-08-27 22:05:19 -0400

    cleaned up euler-5

--- JS/euler-5.js
+++ JS/euler-5.js
@@ -1,18 +1,18 @@
 // Write a function that returns true if a number is divisible by 1..20, then call that function in your loop.
 
-let isDivisible = (x) => {
+let isDivisible = num => {
   let maxDivisor = 20;
   let i = 2;
-   x += maxDivisor;
+   num += maxDivisor;
 
     // while n is divisible by i
     // and i is less than the largest divisor
-    while (x % i === 0 && i <= maxDivisor) {
-      //console.log(x);
+    while (num % i === 0 && i <= maxDivisor) {
+
       // if i is equal to the largest divisor
       if (i === maxDivisor) {
         // then we've found the answer
-        console.log(x);
+        console.log(num);
        return true;
       }
       i++;
@@ -20,54 +20,11 @@ let isDivisible = (x) => {
 };
 
 let euler5 = () => {
-  let x = 0;
+  let num = 0;
 
-  while(!isDivisible(x)) {
-    x += 20;
+  while(!isDivisible(num)) {
+    num += 20;
   }
 };
 
 euler5();
-
-
-// let noRemainder = () => {
-//   let num = 20;
-//
-//   // I could use a function generator/ iterator here with an open ended while loop
-//
-//   while(num >=20) {
-//    //console.log(num);
-//     if (num % 20 == 0) {
-//       let temp =0;
-//       for ( let i = 20; i >= 2; i--) {
-//
-//
-//         if(num % i === 0) {
-//           temp += 1;
-//
-//           if(temp == 19) {
-//         		return num;
-//       		}
-//
-//         }
-//         else {
-//           num += 20;
-//         }
-//
-//         if(temp == 19) {
-//         		return num;
-//       	}
-//
-//       }
-//
-//     }
-//     else {
-//       num += 20;
-//
-//     }
-//
-//   }
-//   console.log('final num '+ num);
-//   //return num;
-// }
-// noRemainder();
