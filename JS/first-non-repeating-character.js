e2247df8bcc407ea7a34443501992e48513d4ff3
Max Ehnert <max.ehnert@gmail.com>
2015-09-07 21:48:08 -0400

    added new problem

---
+++ JS/first-non-repeating-character.js
@@ -0,0 +1,34 @@
+/*
+ * Find the first no repeating character in a string
+*/
+
+let norepeat = str => {
+
+  /*
+   * Make all the letters lower case to avoid any of that.
+   * Split the string into individual characters
+  */
+  var str = str.toLowerCase().split(''); //['a','b','c'];
+  var lastChar = '';
+  // Iterate over the array of characters
+  for ( let char of str ) {
+    // Set the first char to our comparator
+    if (!lastChar) {
+      lastChar = char;
+    }
+    while(lastChar) {
+      /*
+       * As long as the last character stays the same do nothing
+       * Skip blank spaces.
+      */
+      if( lastChar == char || char == ' ' ) {
+        break;
+      }else {
+        return console.log('this 1 dif '+ char);
+        break;
+      }
+    };
+  };
+};
+
+norepeat('aa aaAb');
