0c9c366b17a5d3382d5b192a8e205ba434ac307f
Max Ehnert <max.ehnert@gmail.com>
2015-09-12 22:55:32 -0400

    update BST to es6 class

--- JS/Binary-Search-Tree.js
+++ JS/Binary-Search-Tree.js
@@ -566,3 +566,88 @@ BinarySearchTree.prototype = {
     }
 
 };
+
+
+/************************ rework in es6 *****************/
+class BinarySearchTree {
+
+    //restore constructor
+  //  constructor: BinarySearchTree,
+    constructor() {
+            this._root = null;
+    };
+
+    add (value) {
+        //create a new item object, place data in
+        var node = {
+                value: value,
+                left: null,
+                right: null
+            },
+
+            //used to traverse the structure
+            current;
+
+        //special case: no items in the tree yet
+        if (this._root === null){
+            this._root = node;
+        } else {
+            current = this._root;
+
+            while(true){
+
+                //if the new value is less than this node's value, go left
+                if (value < current.value){
+
+                    //if there's no left, then the new node belongs there
+                    if (current.left === null){
+                        current.left = node;
+                        break;
+                    } else {
+                        current = current.left;
+                    }
+
+                //if the new value is greater than this node's value, go right
+                } else if (value > current.value){
+
+                    //if there's no right, then the new node belongs there
+                    if (current.right === null){
+                        current.right = node;
+                        break;
+                    } else {
+                        current = current.right;
+                    }
+
+                //if the new value is equal to the current one, just ignore
+                } else {
+                    break;
+                }
+            }
+        }
+    };
+
+    contains (value){
+        var found       = false,
+            current     = this._root
+
+        //make sure there's a node to search
+        while(!found && current){
+
+            //if the value is less than the current node's, go left
+            if (value < current.value){
+                current = current.left;
+
+            //if the value is greater than the current node's, go right
+            } else if (value > current.value){
+                current = current.right;
+
+            //values are equal, found it!
+            } else {
+                found = true;
+            }
+        }
+
+        //only proceed if the node was found
+        return found;
+    }
+};
