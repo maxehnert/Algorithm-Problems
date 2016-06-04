6f7619d391087c2a5ccb1514bc2e4be066557f64
Max Ehnert <max.ehnert@gmail.com>
2016-06-04 16:46:48 -0500

    reverseString more ES6-y

--- JS/reverse-string.js
+++ JS/reverse-string.js
@@ -3,12 +3,13 @@ let reverseString = str => {
  str = str.split('');
   let len = str.length -1;
   let half_index = Math.floor( len / 2 );
-  let reverse_str;
+  // let reverse_str;
 
   for ( let i = 0; i <= half_index; i++ ) {
-    reverse_str = str[len - i];
-    str[len -i] = str[i];
-    str[i] = reverse_str;
+    // reverse_str = str[len - i];
+    // str[len -i] = str[i];
+    // str[i] = reverse_str;
+    [str[i], str[len - i]] = [str[len - i], str[i]];
   }
   console.log(str.join(''));
   return str.join('');
