30f02685b5996c3768ac2d392659022f1bd9497c
Max Ehnert <max.ehnert@gmail.com>
2016-06-05 16:03:28 -0500

    Added another palindrome checker.

---
+++ JS/can-be-palindrome.js
@@ -0,0 +1,24 @@
+const canBePalindrome(str) => {
+  str = str.toLowerCase();
+  let letterCounts = {};
+  let letter;
+  let palindromeSum = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    letter = str[i];
+    letterCounts[letter] = letterCounts[letter] || 0;
+    letterCounts[letter]++;
+  }
+
+  for (value in letterCounts) {
+    palindromeSum += letterCounts[value] % 2;
+  }
+};
+
+
+canBePalindrome('aaa'); // true
+canBePalindrome('aa1'); // true
+canBePalindrome('aA1'); // true
+canBePalindrome('aabb'); // true
+canBePalindrome('aabbc'); // true
+canBePalindrome('abbc'); // false
