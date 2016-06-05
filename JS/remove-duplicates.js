ff52fd712712c93aa51d52e58de457c0a066b46f
Max Ehnert <max.ehnert@gmail.com>
2016-06-05 16:42:18 -0500

    Added another removedup fn for arrays.

--- JS/remove-duplicates.js
+++ JS/remove-duplicates.js
@@ -15,7 +15,7 @@ let removeDups = (str) => {
   */
   while( i-- ) {
 
-    // Set our letter check for the first character 
+    // Set our letter check for the first character
     if( !tempLetter ) {
       tempLetter = str[i];
       continue;
@@ -33,3 +33,28 @@ let removeDups = (str) => {
 }
 
 removeDups('cbacdcbc');
+
+/*** Remove dups for arrays ***/
+function removeDuplicate(arr) {
+    var outArr = [];
+
+    for(var i = 0; i < arr.length; i++) {
+
+      // if the number ISN'T already there
+      if(outArr.indexOf(arr[i]) === -1) {
+        // put the new number into the final array
+          outArr.push(arr[i]);
+      }
+    }
+
+    // now use the sort method
+    outArr.sort( function(a,b) {
+      // use a comparator or else it will return numbers lexicographically [1,11,2,245,3]
+      return a - b;
+    });
+console.log(outArr);
+    return outArr;
+
+}
+
+removeDuplicate([1,3,324,3,11,5,6,2,2,3,7,8,1]); // [1, 2, 3, 5, 6, 7, 8, 11, 324]
