5006d426190b0d2c10db411e003ed07352670f11
Max Ehnert <max.ehnert@gmail.com>
2016-09-15 22:47:05 -0500

    Added another method to duplicate array values problem.

--- JS/duplicateArrayValues.js
+++ JS/duplicateArrayValues.js
@@ -19,6 +19,24 @@
     return self;
 };
 
+/**
+ * Another way of doing this
+ */
+Object.defineProperty(Array.prototype, 'duplicate', {
+  value() {
+    var self = this;
+    var arrCopy = Array.from(self);
+    var len = arrCopy.length;
+
+    for (var i = 0; i < len; i++) {
+      self.splice((arrCopy[i] + i), 0, arrCopy[i]);
+    }
+
+    return self;
+  }
+});
+
+
 var arr = [1,2,3,4,5];
 
 arr.duplicate(); // -> [1,1,2,2,3,3,4,4,5,5]
